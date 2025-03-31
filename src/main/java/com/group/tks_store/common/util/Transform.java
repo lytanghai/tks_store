7fb813888ccb16895631830f517cc3894572ff1e
Tanghai Ly <tanghai.ly@wingbank.com.kh>
2025-03-31 08:50:38 +0700

    update ui on create and update pro: variant attributes

--- src/main/java/com/group/tks_store/common/util/Transform.java
+++ src/main/java/com/group/tks_store/common/util/Transform.java
@@ -8,37 +8,6 @@ import java.util.Iterator;
 
 public class Transform {
 
-    // Utility method to convert camelCase to snake_case
-    private static String camelToSnake(String str) {
-        return str.replaceAll("([a-z])([A-Z])", "$1_$2").toLowerCase();
-    }
-
-    // Method to recursively convert JSONObject keys to snake_case
-    public static JSONObject convertKeysToSnakeCase(JSONObject jsonObject) {
-        JSONObject newJsonObject = new JSONObject();
-        Iterator<String> keys = jsonObject.keys();
-
-        while (keys.hasNext()) {
-            String key = keys.next();
-            Object value = jsonObject.get(key);
-
-            // Convert key to snake_case
-            String snakeKey = camelToSnake(key);
-
-            // If value is a JSONObject, recursively convert it
-            if (value instanceof JSONObject) {
-                value = convertKeysToSnakeCase((JSONObject) value);
-            }
-            // If value is a JSONArray, iterate over and convert each element
-            else if (value instanceof JSONArray) {
-                value = convertArrayKeysToSnakeCase((JSONArray) value);
-            }
-
-            // Put the transformed key and value in the new JSONObject
-            newJsonObject.put(snakeKey, value);
-
-public class Transform {
-
     public static JSONObject convertKeysToSnakeCase(JSONObject jsonObject) {
         JSONObject newJsonObject = new JSONObject();
 
